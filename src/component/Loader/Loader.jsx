09e8b7c3c1d2747ab803122114eeaf7ca3c56fe1
Tino (Long) Phan <long.phanphi.tpv@one-line.com>
2024-11-21 11:48:35 +0700

    update loader icon

--- src/component/Loader/Loader.jsx
+++ src/component/Loader/Loader.jsx
@@ -1,11 +1,32 @@
 import "./Loader.css";
 
-const Loader = () => {
+function Loader() {
   return (
-    <div className="bg-black/30 w-full h-[100vh] flex justify-center items-center z-[999] fixed overflow-hidden top-0">
-      <div className="loader"></div>
+    <div className="bg-gray-400/30 w-full h-[100vh] flex justify-center items-center z-[999] fixed overflow-hidden top-0">
+      <div
+        aria-label="orange and tan hamster running in a metal wheel"
+        role="img"
+        className="wheel-and-hamster"
+      >
+        <div className="wheel" />
+        <div className="hamster">
+          <div className="hamster__body">
+            <div className="hamster__head">
+              <div className="hamster__ear" />
+              <div className="hamster__eye" />
+              <div className="hamster__nose" />
+            </div>
+            <div className="hamster__limb hamster__limb--fr" />
+            <div className="hamster__limb hamster__limb--fl" />
+            <div className="hamster__limb hamster__limb--br" />
+            <div className="hamster__limb hamster__limb--bl" />
+            <div className="hamster__tail" />
+          </div>
+        </div>
+        <div className="spoke" />
+      </div>
     </div>
   );
-};
+}
 
 export default Loader;
